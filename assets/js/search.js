3ed517053f075c0b377a203c8cdad298d3fc677a
Damien FREMONT <damien.fremont@gmail.com>
2026-01-10 16:41:42 +0100

    feat: 🎸 faster search with cached json file

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,7 @@
 // REF: https://www.stephanmiller.com/static-site-search/
 
 (function() {
+
     function showResults(results, store) {
       var searchResults = document.getElementById('search-results');
   
@@ -31,12 +32,8 @@
         }
       }
     }
-  
-    var searchTerm = getQuery('query');
-  
-    if (searchTerm) {
-      document.getElementById('search-box').setAttribute("value", searchTerm);
-  
+
+    function searchResult(json) {
       // Initalize lunr.js with the fields to search.
       // The title field is given more weight with the "boost" parameter
       var idx = lunr(function () {
@@ -46,19 +43,30 @@
         this.field('category');
         this.field('content');
   
-        for (var key in window.store) { // Add the JSON we generated from the site content to Lunr.js.
+        for (var key in json) { // Add the JSON we generated from the site content to Lunr.js.
           this.add({
             'id': key,
-            'title': window.store[key].title,
-            'author': window.store[key].author,
-            'category': window.store[key].category,
-            'content': window.store[key].content
+            'title': json[key].title,
+            'author': json[key].author,
+            'category': json[key].category,
+            'content': json[key].content
           });
         }
       });
   
       var results = idx.search(searchTerm); // Perform search with Lunr.js
-      showResults(results, window.store);
+      showResults(results, json);
     }
+
+    // INIT
+    var searchTerm = getQuery('query');
+    if (searchTerm) {
+      document.getElementById('search-box').setAttribute("value", searchTerm);
+      fetch("/search-index.json")
+        .then(response => response.json())
+        .then(json => searchResult(json))
+        .catch(error => console.log(error));
+    }
+
   })();
   
--- search.md
+++ search.md
@@ -17,22 +17,6 @@ permalink: /search/
   <ul id="search-results"></ul>
 </div>
 
-<script>
-  // Template to generate the JSON to search
-  window.store = {
-    {% for post in site.posts %}
-      "{{ post.url | slugify }}": {
-        "title": "{{ post.title | xml_escape }}",
-        "author": "{{ post.author | xml_escape }}",
-        "category": "{{ post.category | xml_escape }}",
-        "content": {{ post.content | strip_html | strip_newlines | jsonify }},
-        "url": "{{ post.url | xml_escape }}"
-      }
-      {% unless forloop.last %},{% endunless %}
-    {% endfor %}
-  };
-</script>
-
 <!-- Import lunr.js from unpkg.com -->
 <script src="/assets/js/lunr-2.3.9.js"></script>
 <!-- Custom search script which we will create below -->
