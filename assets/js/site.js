517f1216a94fbde6d2329ffebe807a3bb428e213
Damien FREMONT <damien.fremont@gmail.com>
2026-07-09 21:36:42 +0200

    feat: 🎸 integrate theme switcher with icon and simplify theme handling logic

--- assets/js/site.js
+++ assets/js/site.js
@@ -12,7 +12,6 @@
     if (storedTheme) {
       return storedTheme
     }
-
     // return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     return 'dark'; // FORCE DEFAULT
   }
@@ -27,34 +26,6 @@
 
   setTheme(getPreferredTheme())
 
-  const showActiveTheme = (theme, focus = false) => {
-    const themeSwitcher = document.querySelector('#bd-theme')
-
-    if (!themeSwitcher) {
-      return
-    }
-
-    const themeSwitcherText = document.querySelector('#bd-theme-text')
-    const activeThemeIcon = document.querySelector('.theme-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-
-    document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-      element.classList.remove('active')
-      element.setAttribute('aria-pressed', 'false')
-    })
-
-    btnToActive.classList.add('active')
-    btnToActive.setAttribute('aria-pressed', 'true')
-    activeThemeIcon.setAttribute('href', svgOfActiveBtn)
-    const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
-    themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
-
-    if (focus) {
-      themeSwitcher.focus()
-    }
-  }
-
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
     const storedTheme = getStoredTheme()
     if (storedTheme !== 'light' && storedTheme !== 'dark') {
@@ -63,16 +34,12 @@
   })
 
   window.addEventListener('DOMContentLoaded', () => {
-    showActiveTheme(getPreferredTheme())
 
-    document.querySelectorAll('[data-bs-theme-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const theme = toggle.getAttribute('data-bs-theme-value')
+    document.querySelector('#bd-theme').addEventListener('click', () => {
+          const storedTheme = getStoredTheme()
+          const theme = (storedTheme == 'light') ? 'dark' : 'light'
           setStoredTheme(theme)
           setTheme(theme)
-          showActiveTheme(theme, true)
         })
-      })
   })
 })()
